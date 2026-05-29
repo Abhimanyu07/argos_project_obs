45653c871e8630f2359c4c65a2f87de7c74d1f46
Abhimanyu07 <harshitjadli44@gmail.com>
2026-05-30 01:10:39 +0200

    stop and go behavior testing

--- controllers/stop_and_go.cpp
+++ controllers/stop_and_go.cpp
@@ -9,9 +9,11 @@ void CStopAndGo::Init(TConfigurationNode& t_node) {
     // Initialization code goes here
     m_pcWheels = GetActuator<CCI_DifferentialSteeringActuator>("differential_steering");
     m_pcProximity = GetSensor<CCI_FootBotProximitySensor>("footbot_proximity");
-
+    Camera = GetSensor<CCI_ColoredBlobOmnidirectionalCameraSensor>("colored_blob_omnidirectional_camera");
+    LEDs = GetActuator<CCI_LEDsActuator>("leds");
     myStopTime = 0;  
     isStopped = false; 
+    Camera->Enable();
 }
 
 void CStopAndGo::ControlStep() {
@@ -19,12 +21,13 @@ void CStopAndGo::ControlStep() {
     const CCI_FootBotProximitySensor::TReadings& proxReadings = m_pcProximity->GetReadings();
     UInt16 MAXSTOP = 10; 
     Real frontReading = std::max({proxReadings[23].Value, proxReadings[0].Value, proxReadings[1].Value}); // 30 degree front cone
-
-
+    Real leftReading = std::max({proxReadings[5].Value, proxReadings[6].Value, proxReadings[7].Value}); // 30 degree left cone
+    Real rightReading = std::max({proxReadings[17].Value, proxReadings[18].Value, proxReadings[19].Value}); // 30 degree right cone
     Real leftSpeed = m_fTargetSpeed; 
     Real rightSpeed = m_fTargetSpeed;
 
     if (isStopped) {
+        LEDs->SetAllColors(CColor::RED);
         myStopTime--;
         m_pcWheels->SetLinearVelocity(0.0f, 0.0f); // stop commands to the actuators 
         if (myStopTime == 0) {
@@ -32,11 +35,18 @@ void CStopAndGo::ControlStep() {
             m_pcWheels->SetLinearVelocity(leftSpeed, -rightSpeed);
         }
     }
-    
-    if(!isStopped) {
-        if (frontReading > 0.1f) {
+
+    else{
+        LEDs->SetAllColors(CColor::GREEN);
+        if (frontReading > 0.05f) {
             myStopTime = MAXSTOP;
             isStopped = true;
+        }
+        else if (leftReading > 0.1f) {
+            m_pcWheels->SetLinearVelocity(leftSpeed*1.5f, 0.0f); // turn right if obstacle is too close on the left
+        }
+        else if (rightReading > 0.1f) {
+            m_pcWheels->SetLinearVelocity(0.0f, rightSpeed*1.5f); // turn left if obstacle is too close on the right
         }
         else{
             m_pcWheels->SetLinearVelocity(leftSpeed, rightSpeed); // go forward if no obstacle in front
--- controllers/stop_and_go.h
+++ controllers/stop_and_go.h
@@ -4,7 +4,8 @@
 #include <argos3/core/control_interface/ci_controller.h>
 #include <argos3/plugins/robots/generic/control_interface/ci_differential_steering_actuator.h>
 #include <argos3/plugins/robots/foot-bot/control_interface/ci_footbot_proximity_sensor.h>
-
+#include <argos3/plugins/robots/generic/control_interface/ci_leds_actuator.h>
+#include <argos3/plugins/robots/generic/control_interface/ci_colored_blob_omnidirectional_camera_sensor.h>
 using namespace argos;
 
 class CStopAndGo : public CCI_Controller {
@@ -24,6 +25,8 @@ private:
     Real m_fTargetSpeed;
     UInt16 myStopTime;  
     bool isStopped; 
+    CCI_LEDsActuator* LEDs;
+    CCI_ColoredBlobOmnidirectionalCameraSensor* Camera;
 };
 
 #endif
